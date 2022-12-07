70f1b71c626784a9c4b52ce35c07a66e541f7635
minaje0917 <k050918@naver.com>
2022-12-07 14:52:39 +0900

    :tada: Start idea

---
+++ idea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idea
+//
+//  Created by 선민재 on 2022/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
