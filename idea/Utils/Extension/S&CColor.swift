2684b5c4243926d516bf1ad49f40dfeaa91de6d9
minaje0917 <k050918@naver.com>
2022-12-20 20:52:07 +0900

    :zap: add Major Color

--- idea/Utils/Extension/S&CColor.swift
+++ idea/Utils/Extension/S&CColor.swift
@@ -11,5 +11,12 @@ extension UIColor {
     static let Main = UIColor(named: "Main")
     static let Background = UIColor(named: "Background")
     static let LightMain = UIColor(named: "LightMain")
+    static let FE = UIColor(named: "FE")
+    static let BE = UIColor(named: "BE")
+    static let iOS = UIColor(named: "iOS")
+    static let AOS = UIColor(named: "AOS")
+    static let Study = UIColor(named: "Study")
+    static let Con = UIColor(named: "Con")
+    static let Etc = UIColor(named: "Etc")
 }
 
