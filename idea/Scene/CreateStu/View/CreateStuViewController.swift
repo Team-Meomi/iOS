663d17087bf4eab76af02cc6459f9d8f0e7ed5df
minaje0917 <k050918@naver.com>
2022-12-23 13:36:40 +0900

    :sparkles: finish CreateStu publishing

--- idea/Scene/CreateStu/View/CreateStuViewController.swift
+++ idea/Scene/CreateStu/View/CreateStuViewController.swift
@@ -5,12 +5,26 @@
 //  Created by 선민재 on 2022/12/20.
 //
 
+//
+//  CreateConViewController.swift
+//  idea
+//
+//  Created by 선민재 on 2022/12/20.
+//
+
 import UIKit
 import Then
 import SnapKit
 
 class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextViewDelegate{
-
+    
+    var postMajor:String = ""
+    var guardFeTap:Bool = false
+    var guardBeTap:Bool = false
+    var guardiOSTap:Bool = false
+    var guardAosTap:Bool = false
+    var guardEtcTap:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "스터디"
@@ -39,12 +53,12 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.textAlignment = .left
     }
     
-    func placeholderSetting() {
+    private func placeholderSetting() {
         explainTextView.delegate = self
         explainTextView.text = "설명"
         explainTextView.textColor = UIColor.lightGray
     }
-        
+    
     func textViewDidBeginEditing(_ textView: UITextView) {
         if explainTextView.textColor == UIColor.lightGray {
             explainTextView.text = nil
@@ -58,21 +72,6 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
             explainTextView.textColor = UIColor.lightGray
         }
     }
-
-    lazy var selectDateBtn = UIButton().then {
-        let text = NSAttributedString(string: "  날짜를 선택해주세요")
-        $0.setAttributedTitle(text, for: .normal)
-        $0.titleLabel?.font = UIFont.SCFont(size: 14, family: .Medium)
-        $0.setTitleColor(UIColor.black, for: .normal)
-        $0.backgroundColor = .white
-        $0.layer.cornerRadius = 8
-        $0.layer.borderColor = UIColor.Main?.cgColor
-        $0.layer.borderWidth = 1
-        $0.setImage(UIImage(systemName: "calendar"), for: .normal)
-        $0.tintColor = .Main
-//        $0.addTarget(self, action: #selector(signUpBtnDidTap), for: .touchUpInside)
-    }
-    
     lazy var feBtn = UIButton().then {
         let text = NSAttributedString(string: "FE")
         $0.setAttributedTitle(text, for: .normal)
@@ -82,6 +81,7 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
         $0.layer.borderColor = UIColor.Main?.cgColor
         $0.layer.borderWidth = 1
+        $0.addTarget(self, action: #selector(feBtnDidTap), for: .touchUpInside)
     }
     
     lazy var beBtn = UIButton().then {
@@ -93,6 +93,7 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
         $0.layer.borderColor = UIColor.Main?.cgColor
         $0.layer.borderWidth = 1
+        $0.addTarget(self, action: #selector(beBtnDidTap), for: .touchUpInside)
     }
     
     lazy var iosBtn = UIButton().then {
@@ -104,6 +105,7 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
         $0.layer.borderColor = UIColor.Main?.cgColor
         $0.layer.borderWidth = 1
+        $0.addTarget(self, action: #selector(iOSBtnDidTap), for: .touchUpInside)
     }
     
     lazy var aosBtn = UIButton().then {
@@ -115,6 +117,7 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
         $0.layer.borderColor = UIColor.Main?.cgColor
         $0.layer.borderWidth = 1
+        $0.addTarget(self, action: #selector(aosBtnDidTap), for: .touchUpInside)
     }
     
     lazy var etcBtn = UIButton().then {
@@ -126,6 +129,7 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
         $0.layer.borderColor = UIColor.Main?.cgColor
         $0.layer.borderWidth = 1
+        $0.addTarget(self, action: #selector(etcBtnDidTap), for: .touchUpInside)
     }
     
     lazy var makeBtn = UIButton().then {
@@ -137,8 +141,134 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
         $0.layer.cornerRadius = 8
     }
     
+    private let selectText = UILabel().then {
+        $0.text = "날짜를 선택해 주세요"
+        $0.textColor = UIColor(red: 163/255, green: 163/255, blue: 163/255, alpha: 1.00)
+        $0.font = UIFont.SCFont(size:14,family:.Medium)
+    }
+    
+    private let calenderIcon = UIImageView().then {
+        $0.image = UIImage(systemName: "calendar")
+        $0.tintColor = .Main
+    }
+    
+    private let selectView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 8
+        $0.layer.borderColor = UIColor.Main?.cgColor
+        $0.layer.borderWidth = 1
+    }
+    
+    private let datePicker = UIDatePicker().then {
+        $0.preferredDatePickerStyle = .compact
+        $0.datePickerMode = .date
+        $0.locale = Locale(identifier: "ko-KR")
+        $0.timeZone = .autoupdatingCurrent
+        $0.translatesAutoresizingMaskIntoConstraints = true
+        $0.addTarget(self, action: #selector(handleDatePicker), for: .valueChanged)
+    }
+    
+    
+    @objc func feBtnDidTap() {
+        guard guardFeTap != true else{
+            feBtn.setTitleColor(UIColor(red: 82/255, green: 82/255, blue: 87/255, alpha: 1), for: .normal)
+            feBtn.layer.borderColor = UIColor.Main?.cgColor
+            feBtn.layer.borderWidth = 1
+            feBtn.backgroundColor = .white
+            self.postMajor = ""
+            self.guardFeTap = false
+            return
+        }
+        guard guardBeTap != true && guardiOSTap != true && guardAosTap != true && guardEtcTap != true else{return}
+        feBtn.layer.borderColor = UIColor.FE?.cgColor
+        feBtn.backgroundColor = .FE
+        feBtn.setTitleColor(UIColor.white,for: .normal)
+        self.postMajor = "FE"
+        self.guardFeTap = true
+    }
+    
+    @objc func beBtnDidTap() {
+        guard guardBeTap != true else{
+            beBtn.setTitleColor(UIColor(red: 82/255, green: 82/255, blue: 87/255, alpha: 1), for: .normal)
+            beBtn.layer.borderColor = UIColor.Main?.cgColor
+            beBtn.layer.borderWidth = 1
+            beBtn.backgroundColor = .white
+            self.postMajor = ""
+            self.guardBeTap = false
+            return
+        }
+        guard guardFeTap != true && guardiOSTap != true && guardAosTap != true && guardEtcTap != true else{return}
+        beBtn.layer.borderColor = UIColor.BE?.cgColor
+        beBtn.backgroundColor = .BE
+        beBtn.setTitleColor(UIColor.white,for: .normal)
+        self.postMajor = "BE"
+        self.guardBeTap = true
+    }
+    
+    @objc func iOSBtnDidTap() {
+        guard guardiOSTap != true else{
+            iosBtn.setTitleColor(UIColor(red: 82/255, green: 82/255, blue: 87/255, alpha: 1), for: .normal)
+            iosBtn.layer.borderColor = UIColor.Main?.cgColor
+            iosBtn.layer.borderWidth = 1
+            iosBtn.backgroundColor = .white
+            self.postMajor = ""
+            self.guardiOSTap = false
+            return
+        }
+        guard guardFeTap != true && guardBeTap != true && guardAosTap != true && guardEtcTap != true else{return}
+        iosBtn.layer.borderColor = UIColor.iOS?.cgColor
+        iosBtn.backgroundColor = .iOS
+        iosBtn.setTitleColor(UIColor.white,for: .normal)
+        self.postMajor = "iOS"
+        self.guardiOSTap = true
+    }
+    
+    @objc func aosBtnDidTap() {
+        guard guardAosTap != true else{
+            aosBtn.setTitleColor(UIColor(red: 82/255, green: 82/255, blue: 87/255, alpha: 1), for: .normal)
+            aosBtn.layer.borderColor = UIColor.Main?.cgColor
+            aosBtn.layer.borderWidth = 1
+            aosBtn.backgroundColor = .white
+            self.postMajor = ""
+            self.guardAosTap = false
+            return
+        }
+        guard guardFeTap != true && guardBeTap != true && guardiOSTap != true && guardEtcTap != true else{return}
+        aosBtn.layer.borderColor = UIColor.AOS?.cgColor
+        aosBtn.backgroundColor = .AOS
+        aosBtn.setTitleColor(UIColor.white,for: .normal)
+        self.postMajor = "AOS"
+        self.guardAosTap = true
+    }
+    
+    @objc func etcBtnDidTap() {
+        guard guardEtcTap != true else{
+            etcBtn.setTitleColor(UIColor(red: 82/255, green: 82/255, blue: 87/255, alpha: 1), for: .normal)
+            etcBtn.layer.borderColor = UIColor.Main?.cgColor
+            etcBtn.layer.borderWidth = 1
+            etcBtn.backgroundColor = .white
+            self.postMajor = ""
+            self.guardEtcTap = false
+            return
+        }
+        guard guardFeTap != true && guardBeTap != true && guardiOSTap != true && guardAosTap != true else{return}
+        etcBtn.layer.borderColor = UIColor.Etc?.cgColor
+        etcBtn.backgroundColor = .Etc
+        etcBtn.setTitleColor(UIColor.white,for: .normal)
+        self.postMajor = "기타"
+        self.guardEtcTap = true
+    }
+    
+    @objc func handleDatePicker() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        selectText.text = dateFormatter.string(from: datePicker.date)
+        print(datePicker.date)
+    }
+    
+    
     override func addView() {
-        [titleTextField,titleTextFieldLine,explainTextView,selectDateBtn,feBtn,beBtn,iosBtn,aosBtn,etcBtn,makeBtn].forEach{
+        [titleTextField,titleTextFieldLine,explainTextView,selectView,selectText,calenderIcon,datePicker,feBtn,beBtn,iosBtn,aosBtn,etcBtn,makeBtn].forEach{
             view.addSubview($0)
         }
     }
@@ -160,37 +290,51 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
             $0.leading.trailing.equalToSuperview().inset(25)
             $0.height.equalTo((bounds.height) / 7.73)
         }
-        selectDateBtn.snp.makeConstraints {
-            $0.top.equalTo(explainTextView.snp.bottom).offset(31)
+        selectView.snp.makeConstraints {
+            $0.top.equalTo(explainTextView.snp.bottom).offset(20)
             $0.leading.trailing.equalToSuperview().inset(25)
             $0.height.equalTo(40)
         }
+        selectText.snp.makeConstraints {
+            $0.centerY.equalTo(selectView.snp.centerY).offset(0)
+            $0.leading.equalTo(selectView.snp.leading).offset(45)
+        }
+        calenderIcon.snp.makeConstraints {
+            $0.centerY.equalTo(selectView.snp.centerY).offset(0)
+            $0.leading.equalTo(selectView.snp.leading).offset(14)
+            $0.height.equalTo(24)
+            $0.width.equalTo(24)
+        }
+        datePicker.snp.makeConstraints {
+            $0.centerY.equalTo(selectView.snp.centerY).offset(0)
+            $0.trailing.equalTo(selectView.snp.trailing).inset(2)
+        }
         feBtn.snp.makeConstraints {
-            $0.top.equalTo(selectDateBtn.snp.bottom).offset(24)
+            $0.top.equalTo(selectView.snp.bottom).offset(24)
             $0.leading.equalTo(view.snp.leading).offset(26)
             $0.height.equalTo(35)
             $0.width.equalTo(55)
         }
         beBtn.snp.makeConstraints {
-            $0.top.equalTo(selectDateBtn.snp.bottom).offset(24)
+            $0.top.equalTo(selectView.snp.bottom).offset(24)
             $0.leading.equalTo(feBtn.snp.trailing).offset(12)
             $0.height.equalTo(35)
             $0.width.equalTo(55)
         }
         iosBtn.snp.makeConstraints {
-            $0.top.equalTo(selectDateBtn.snp.bottom).offset(24)
+            $0.top.equalTo(selectView.snp.bottom).offset(24)
             $0.leading.equalTo(beBtn.snp.trailing).offset(12)
             $0.height.equalTo(35)
             $0.width.equalTo(55)
         }
         aosBtn.snp.makeConstraints {
-            $0.top.equalTo(selectDateBtn.snp.bottom).offset(24)
+            $0.top.equalTo(selectView.snp.bottom).offset(24)
             $0.leading.equalTo(iosBtn.snp.trailing).offset(12)
             $0.height.equalTo(35)
             $0.width.equalTo(55)
         }
         etcBtn.snp.makeConstraints {
-            $0.top.equalTo(selectDateBtn.snp.bottom).offset(24)
+            $0.top.equalTo(selectView.snp.bottom).offset(24)
             $0.leading.equalTo(aosBtn.snp.trailing).offset(12)
             $0.height.equalTo(35)
             $0.width.equalTo(55)
@@ -201,4 +345,5 @@ class CreateStuViewController: BaseViewController<CreateStuViewModel>,UITextView
             $0.height.equalTo(45)
         }
     }
+
 }
---
+++ idea/Utils/Extension/Network/Main/CheckHomebase.swift
@@ -0,0 +1,33 @@
+//
+//  CheckHomebase.swift
+//  idea
+//
+//  Created by 선민재 on 2022/12/23.
+//
+
+import UIKit
+import Moya
+
+extension CreateStuViewController {
+//    func signUp() {
+//        let number = Int(numberTextField.text!) ?? 0
+//        let param = SignUpRequest.init(self.emailTextField.text! + "@gsm.hs.kr", self.pwTextField.text!, self.nameTextField.text!, number)
+//        print(param)
+//        BaseVC.authProvider.request(.signUp(param: param)) {response in
+//            switch response {
+//            case .success(let result):
+//                let statusCode = result.statusCode
+//                switch statusCode {
+//                case 200..<300:
+//                    self.success()
+//                case 409:
+//                    self.same()
+//                default:
+//                    self.failure()
+//                }
+//            case .failure(let err):
+//                print(err.localizedDescription)
+//            }
+//        }
+//    }
+}
