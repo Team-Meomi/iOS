2d4c0f4077f90b42fe6f73f88145ff631f864638
minaje0917 <k050918@naver.com>
2023-01-02 04:22:38 +0900

    :sparkles: add studyBtn,conBtn add action

--- idea/Scene/Admin/Main/VC/AdminMainVC.swift
+++ idea/Scene/Admin/Main/VC/AdminMainVC.swift
@@ -47,8 +47,43 @@ class AdminMainVC: BaseViewController<AdminMainVM> {
 //        $0.dataSource = ["1학년","2학년","3학년"]
 //        $0.show()
 //    }
+    lazy var conBtn = UIButton().then {
+        let text = NSAttributedString(string: "컨퍼런스")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.SCFont(size: 14, family: .Medium)
+        $0.setTitleColor(UIColor.Main, for: .normal)
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+        $0.layer.cornerRadius = 5
+        $0.addTarget(self, action: #selector(conBtnDidTap), for: .touchUpInside)
+    }
+    
+    lazy var studyBtn = UIButton().then {
+        let text = NSAttributedString(string: "스터디")
+        $0.setAttributedTitle(text, for: .normal)
+        $0.titleLabel?.font = UIFont.SCFont(size: 14, family: .Medium)
+        $0.setTitleColor(UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1), for: .normal)
+        $0.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+        $0.layer.cornerRadius = 5
+        $0.addTarget(self, action: #selector(studyBtnDidTap), for: .touchUpInside)
+    }
+    
+    let divisionLine = UIView().then {
+        $0.backgroundColor = .Main
+        $0.layer.cornerRadius = 4
+    }
+    
+    @objc func conBtnDidTap() {
+        conBtn.setTitleColor(UIColor.Main, for: .normal)
+        studyBtn.setTitleColor(UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1), for: .normal)
+    }
+
+    @objc func studyBtnDidTap() {
+        studyBtn.setTitleColor(UIColor.Main, for: .normal)
+        conBtn.setTitleColor(UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1), for: .normal)
+    }
+    
     override func addView() {
-        [titleDateText,explainText].forEach {
+        [titleDateText,explainText,conBtn,divisionLine,studyBtn].forEach {
             view.addSubview($0)
         }
     }
@@ -61,6 +96,17 @@ class AdminMainVC: BaseViewController<AdminMainVM> {
             explainText.layout
                 .top(.to(titleDateText).bottom, .equal(9))
                 .centerX(.toSuperview())
+            conBtn.layout
+                .top(.to(explainText).bottom, .equal((bounds.height) / 6.15))
+                .leading(.to(view).leading, .equal((bounds.width) / 4.36))
+            divisionLine.layout
+                .top(.to(view).top, .equal(239))
+                .centerX(.toSuperview())
+                .width(3)
+                .height(30)
+            studyBtn.layout
+                .top(.to(explainText).bottom, .equal((bounds.height) / 6.15))
+                .trailing(.to(view).trailing, .equal(-(bounds.width) / 3.9))
         }
     }
 
