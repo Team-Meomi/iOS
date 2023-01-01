f51efeef61f3760db1e5f08c1084d8ca11a14762
minaje0917 <k050918@naver.com>
2023-01-02 03:55:13 +0900

    :sparkles: add DateText, dateFormatter

--- idea/Scene/Admin/Main/VC/AdminMainVC.swift
+++ idea/Scene/Admin/Main/VC/AdminMainVC.swift
@@ -9,24 +9,58 @@ import UIKit
 import MSGLayout
 import Then
 import Foundation
+import DropDown
 
 class AdminMainVC: BaseViewController<AdminMainVM> {
 
+    let nowDate = Date()
+    let dateFormatter = DateFormatter().then {
+        $0.dateFormat = "yyyy-MM-dd"
+    }
+    lazy var nowDateStr = dateFormatter.string(from: nowDate)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.hidesBackButton = true
-        view.backgroundColor = .cyan
     }
-
+    
+    lazy var titleDateText = UILabel().then {
+        $0.text = nowDateStr
+        $0.font = UIFont.SCFont(size: 16,family:.Bold)
+        $0.textColor = .black
+    }
+    
+    let explainText = UILabel().then {
+        $0.text = "컨퍼런스와 스터디 목록입니다"
+        $0.font = UIFont.SCFont(size: 16,family:.Bold)
+        $0.textColor = .black
+    }
+        
+//    let gradeDropDown = DropDown().then {
+//        DropDown.appearance().textColor = UIColor.black // 아이템 텍스트 색상
+//        DropDown.appearance().selectedTextColor = UIColor.blue // 선택된 아이템 텍스트 색상
+//        DropDown.appearance().backgroundColor = UIColor.white // 아이템 팝업 배경 색상
+//        DropDown.appearance().selectionBackgroundColor = UIColor.lightGray // 선택한 아이템 배경 색상
+//        DropDown.appearance().setupCornerRadius(8)
+//        $0.backgroundColor = UIColor.init(named: "#F1F1F1")
+//        $0.layer.cornerRadius = 8
+//        $0.dataSource = ["1학년","2학년","3학년"]
+//        $0.show()
+//    }
     override func addView() {
-        [].forEach {
+        [titleDateText,explainText].forEach {
             view.addSubview($0)
         }
     }
     
     override func setLayout() {
         MSGLayout.buildLayout {
-            
+            titleDateText.layout
+                .top(.to(view).top, .equal(57))
+                .centerX(.toSuperview())
+            explainText.layout
+                .top(.to(titleDateText).bottom, .equal(9))
+                .centerX(.toSuperview())
         }
     }
 
